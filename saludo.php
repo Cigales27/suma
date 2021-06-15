aaafa8a3a894e28465504f5c6786568f594962ea
Cigales27 <72766195+Cigales27@users.noreply.github.com>
2021-06-15 00:17:24 -0500

    prueba de eliminar ssl

--- saludo.php
+++ saludo.php
@@ -1,2 +1,7 @@
 <?php
+function redirectTohttp() {
+if($_SERVER[‘HTTP’]!=”on”) {
+$redirect= 'http://primerappangular.herokuapp.com/saludo.php//'.$_SERVER[‘HTTP_HOST’].$_SERVER[‘REQUEST_URI’];
+header(“Location:$redirect”);
+}}
     echo json_encode (["resultado"=>"Hola mundo"]);
